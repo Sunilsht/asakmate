05a53947719412d5365eeeaae68681ba5fcca0e2
sunil shrestha <explore.sunilshrestha@gmail.com>
2019-12-11 08:52:56 +0545

    models

--- qna_app/models.py
+++ qna_app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+class QuestionModel(models.Model):
+    title=models.CharField(max_length=255)
+    posted_by=models.CharField(max_length=120)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    question_desp=models.TextField()
+    question_img=models.ImageField(upload_to="QuestionImg")
+
+
+class AnswerModel(models.Model):
+    ans_by=models.CharField(max_length=120)
+    timestamp=models.DateTimeField(auto_now_add=True)
+    answer_desp=models.TextField()
+    answers_img=models.ImageField(upload_to="AnswersImg")
+    is_accepted=models.BooleanField()
+    votes =models.models.IntegerField()
+    question=models.ForeignKey(QuestionModel,on_delete=models.CASCADE)
+    
+
+
+
 
 # Create your models here.
---
+++ qna_app/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from qna_app.views import 
+urls_pattern = [
+path('add/',views.addquestion ,"")
+]
